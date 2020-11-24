f083b6dec31b13f7c5335141fc713c5fcfb0ae78
ZiomaleQ <r.partyka30@gmail.com>
2020-11-24 23:46:35 +0100

    Better atak & unik

--- commands/atak.js
+++ commands/atak.js
@@ -11,39 +11,23 @@ module.exports = {
         let embed = new Keiko.Discord.RichEmbed().setTitle('No siemka'), user, sub = [];
         let helpWord = Keiko.interpenter.readWord()
         if (helpWord == "help") {
-            msg.channel.send(embed.addField("Użycie komendy:", `\`keiko!atak <poziom> [dodatkowe obrażenia] [krytyczne] [wartość krytycznego] [drugi atak]\``)
+            msg.channel.send(embed.addField("Użycie komendy:", `\`keiko!atak <poziom> [dodatkowe obrażenia] [modyfikator]\``)
                 .addField("Ogólny opis:", "Licze obrażenia ataku podstawowego!"))
             return
         }
         if (helpWord) Keiko.interpenter.moveByInt(-helpWord.length);
-        sub.push(Keiko.interpenter.readInt()) // lvl
-        if (!sub[0]) throw "nie podałeś mi poziomu"
-        var dice = utils.genRandom(20, 40)
-        sub.push(Keiko.interpenter.readInt()) // dmg
-        sub.push(Keiko.interpenter.readInt()) // kryt
-        sub.push(Keiko.interpenter.readInt()) // wartość kryta
-        if (!sub[4]) sub[4] = 200
-        sub.push(Keiko.interpenter.readInt()) // drugi atak
-        dmg = utils.genRandom(20 + ((sub[0] - 1) * 10), 40 + ((sub[0] - 1) * 10) + sub[1])
-        var mess = ""
-        if (sub[2] > 0) {
-            let temp = utils.genRandom(0, 99);
-
-            if (sub[2] > temp) {
-                dmg = Math.round(sub[4] / 100 * dmg)
-                mess += " trafiłeś krytycznie,"
-            }
+        sub.push(Keiko.interpenter.readInt() - 1, Keiko.interpenter.readInt(), Keiko.interpenter.readInt())
+        if (sub[0] < 0 && sub[0]) throw "poziom jest mniejszy od 0"
+        var okay = utils.genRandom(0, 40) + sub[2]
+        var dmg = utils.genRandom(0, sub[0] * 15) + sub[0] * 15 + sub[1];
+        if (okay >= 15) {
+            msg.channel.send(embed
+                .addField('Informacje:', `[${okay}] Trafiłeś, zadałeś ${dmg} obrażeń.`).setColor('GREEN'));
+            return { user: msg.author.id, outcome: true };
+        } else {
+            msg.channel.send(embed
+                .addField('Informacje:', `[${okay}] Niestety, atak się nie udał...`).setColor('RED'));
+            return { user: msg.author.id, outcome: false };
         }
-        if (sub[4] > 0) {
-            let temp = utils.genRandom(0, 99);
-            if (sub[4] > temp) {
-                dmg *= 2
-                mess += " uderzyłeś ponownie."
-            }
-        }
-        mess = mess.trimLeft()
-
-        msg.channel.send(new Keiko.Discord.RichEmbed().setTitle("Pif paf")
-            .addField(`[${dice}] Wynik ataku`, `${`${mess[0].toUpperCase()}${mess.substr(1)}`}\nZadałeś ${dmg} obrażeń`).setColor("GREEN"))
     }
 }
--- commands/unik.js
+++ commands/unik.js
@@ -8,32 +8,23 @@ module.exports = {
     package: "default",
     execute: async (Keiko, msg) => {
         if (msg.guild.id != "749007879150895105") throw "musisz być na SAO:Reborn"
-        let embed = new Keiko.Discord.RichEmbed().setTitle('No siemka'), user, sub = [];
+        let embed = new Keiko.Discord.RichEmbed().setTitle('No siemka'), sub = [];
         let helpWord = Keiko.interpenter.readWord()
         if (helpWord == "help") {
-            msg.channel.send(embed.addField("Użycie komendy:", `\`keiko!atak <dmg> <pancerz> <unik>\``)
-                .addField("Ogólny opis:", "Licze obrażenia ataku podstawowego!"))
+            msg.channel.send(embed.addField("Użycie komendy:", `\`keiko!atak <unik> <dmg> <pancerz>\``)
+                .addField("Ogólny opis:", "Licze ile dostałeś w tyłek od ataku podstawowego!"))
             return
         }
         if (helpWord) Keiko.interpenter.moveByInt(-helpWord.length);
-        sub.push(Keiko.interpenter.readInt()) // dmg
-        var dice = utils.genRandom(20, 40)
-        sub.push(Keiko.interpenter.readInt()) // pancerz
-        sub.push(Keiko.interpenter.readInt()) // unik
-
-        dmg = Math.round(sub[0] * (100 / (sub[1] + 100)))
-
-        if (sub[2] > 0) {
-            var temp = utils.genRandom(1, 99)
-            if (sub[2] > temp) {
-                msg.channel.send(new Keiko.Discord.RichEmbed().setTitle("Pif paf")
-                    .addField(`[${dice}] Wynik uniku (PvE)`, `Gratulacje jeśli jest to walka przeciwko potworowi unikasz ciosu`)
-                    .addField(`[${dice}] Wynik uniku (PvP)`, `Dostajesz ${dmg} obrażeń`).setColor("GREEN"));
-                return;
-            }
+        var snek = Keiko.interpenter.readInt()
+        sub.push(Keiko.interpenter.readInt(), Keiko.interpenter.readInt())
+        var okay = utils.genRandom(1, 100);
+        if (okay > snek) {
+            let dmg = sub[0];
+            if (sub[1] > 0 && sub[1] < 100) dmg = dmg * (100 / (100 + sub[1]))
+            msg.channel.send(embed.addField('Informacje:', `[${okay}] Niestety, unik się nie udał...\n Otrzymałeś od życia ${Math.floor(dmg)} w tyłek`).setColor('RED'))
+        } else {
+            msg.channel.send(embed.addField('Informacje:', `[${okay}] Twój unik się udał!`).setColor('GREEN'))
         }
-
-        msg.channel.send(new Keiko.Discord.RichEmbed().setTitle("Pif paf")
-            .addField(`[${dice}] Wynik uniku`, `Otrzymałeś ${dmg} obrażeń`).setColor("RED"))
     }
 }
